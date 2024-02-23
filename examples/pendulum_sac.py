cf279adc12d053ca2995e4436cd8cb7e982724db
Jonas Eschmann <jonas.eschmann@gmail.com>
2024-02-23 11:22:31 -0500

    simplifying example

--- examples/pendulum_sac.py
+++ examples/pendulum_sac.py
@@ -1,19 +1,18 @@
 from tinyrl import loop_sac
 import gymnasium as gym
 
-if __name__ == "__main__":
-    def env_factory():
-        return gym.make("Pendulum-v1")
+def env_factory():
+    return gym.make("Pendulum-v1")
 
-    loop = loop_sac(env_factory)
-    finished = False
-    step = 0
+loop = loop_sac(env_factory)
+finished = False
+step = 0
 
-    loop_state = loop.LoopState()
+loop_state = loop.LoopState()
 
-    loop.init(loop_state, 0)
-    while not finished:
-        if(step % 100 == 0):
-            print(f"Step {step}")
-        finished = loop.step(loop_state)
-        step += 1
+loop.init(loop_state, 0)
+while not finished:
+    if(step % 100 == 0):
+        print(f"Step {step}")
+    finished = loop.step(loop_state)
+    step += 1
