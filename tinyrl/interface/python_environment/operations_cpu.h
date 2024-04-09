9da2574facb844c9107f9f8c74b5fd28a3189948
Jonas Eschmann <jonas.eschmann@gmail.com>
2024-04-09 15:44:07 -0400

    0.2.1

--- tinyrl/interface/python_environment/operations_cpu.h
+++ tinyrl/interface/python_environment/operations_cpu.h
@@ -2,6 +2,37 @@
 
 std::function<pybind11::object()> environment_factory;
 
+template <typename SPEC>
+void observe_state(pybind11::array observation_array, typename PythonEnvironment<SPEC>::State& state){
+    using T = typename SPEC::T;
+    // pybind11::array observation_array = observation.template cast<pybind11::array>();
+    pybind11::buffer_info observation_info = observation_array.request();
+    bool observation_is_float = observation_info.format == pybind11::format_descriptor<float>::format();
+    bool observation_is_double = observation_info.format == pybind11::format_descriptor<double>::format();
+    if (!observation_is_float && !observation_is_double){
+        throw std::runtime_error(std::string("Incompatible buffer format: [") + observation_info.format + std::string("]. Check the floating point type of the observation returned by env.step() and the one configured when building the TinyRL interface"));
+    } 
+    if(observation_info.ndim != 1){
+        throw std::runtime_error("Incompatible buffer format. Check the dimension of the observation returned by env.step() and the one configured when building the TinyRL interface");
+    }
+
+    size_t num_elements = observation_info.shape[0];
+    if(num_elements != SPEC::OBSERVATION_DIM){
+        throw std::runtime_error("Incompatible observation dimension. Check the dimension of the observation returned by env.step() and the one configured when building the TinyRL interface");
+    }
+
+    if(observation_info.format == pybind11::format_descriptor<T>::format()){
+        auto observation_data_ptr = static_cast<T*>(observation_info.ptr);
+        std::copy(observation_data_ptr, observation_data_ptr + num_elements, state.state.begin());
+    }
+    else{
+        auto observation_data_ptr = static_cast<double*>(observation_info.ptr);
+        for(size_t i=0; i<SPEC::OBSERVATION_DIM; i++){
+            state.state[i] = observation_data_ptr[i];
+        }
+    }
+}
+
 namespace rl_tools{
     template<typename DEVICE, typename SPEC>
     static void malloc(DEVICE& device, PythonEnvironment<SPEC>& env){
@@ -17,17 +48,8 @@ namespace rl_tools{
         auto result = env.environment->attr("reset")();
         pybind11::tuple result_tuple = pybind11::cast<pybind11::tuple>(result);
         auto first_element = result_tuple[0];
-        pybind11::array array = first_element.cast<pybind11::array>();
-        pybind11::buffer_info info = array.request();
-        if (info.format != pybind11::format_descriptor<T>::format() || info.ndim != 1) {
-            throw std::runtime_error("Incompatible buffer format. Check the floating point type of the observation returned by env.reset() and the one configured when building the TinyRL interface");
-        }
-        auto data_ptr = static_cast<T*>(info.ptr);
-        size_t num_elements = info.shape[0];
-        if(num_elements != SPEC::OBSERVATION_DIM){
-            throw std::runtime_error("Incompatible observation dimension. Check the dimension of the observation returned by env.reset() and the one configured when building the TinyRL interface");
-        }
-        std::copy(data_ptr, data_ptr + num_elements, state.state.begin());
+        pybind11::array observation_array = first_element.cast<pybind11::array>();
+        observe_state<SPEC>(observation_array, state);
         state.terminated = false;
         state.reward = 0;
     }
@@ -55,17 +77,8 @@ namespace rl_tools{
         auto truncated = result_tuple[3];
 
         pybind11::array observation_array = observation.cast<pybind11::array>();
-        pybind11::buffer_info observation_info = observation_array.request();
-        if (observation_info.format != pybind11::format_descriptor<T>::format() || observation_info.ndim != 1) {
-            throw std::runtime_error("Incompatible buffer format. Check the floating point type of the observation returned by env.step() and the one configured when building the TinyRL interface");
-        }
+        observe_state<SPEC>(observation_array, next_state);
 
-        auto observation_data_ptr = static_cast<T*>(observation_info.ptr);
-        size_t num_elements = observation_info.shape[0];
-        if(num_elements != SPEC::OBSERVATION_DIM){
-            throw std::runtime_error("Incompatible observation dimension. Check the dimension of the observation returned by env.step() and the one configured when building the TinyRL interface");
-        }
-        std::copy(observation_data_ptr, observation_data_ptr + num_elements, next_state.state.begin());
         next_state.reward = reward.cast<T>();
         next_state.terminated = terminated.cast<bool>();
         return 0;
