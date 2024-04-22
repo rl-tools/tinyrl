b0a4d532cdb1d945d0b3c61cbdc7859078da60a9
Jonas Eschmann <jonas.eschmann@gmail.com>
2024-04-22 13:52:33 -0400

    -lblas actually works

--- tinyrl/src/accelerate.py
+++ tinyrl/src/accelerate.py
@@ -1,17 +1,23 @@
-import sys
+import sys, os
 from .link_accelerate import link_accelerate
 from .link_mkl import link_mkl
 from .link_blas import link_blas
 
-def acceleration_flags(module, disable_mkl=True):
+disable_mkl = "TINYRL_DISABLE_MKL" in os.environ
+
+def acceleration_flags(module):
     flags = []
+    success = False
     if sys.platform == "darwin":
-        flags += link_accelerate()
+        success, accelerate_flags = link_accelerate()
+        if success:
+            flags += accelerate_flags
     elif sys.platform == "linux":
-        success, mkl_flags = link_mkl()
-        if success and not disable_mkl:
-            flags += mkl_flags
-        else:
+        if not disable_mkl:
+            success, mkl_flags = link_mkl()
+            if success:
+                flags += mkl_flags
+        if not success:
             success, blas_flags = link_blas(module)
             flags += blas_flags
     return flags
