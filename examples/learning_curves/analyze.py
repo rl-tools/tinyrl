f5bc4f772586210d2de61d589f10ebd3379a4482
Jonas Eschmann <jonas.eschmann@gmail.com>
2024-04-23 21:54:04 -0400

    updating plotting script

--- examples/learning_curves/analyze.py
+++ examples/learning_curves/analyze.py
@@ -26,7 +26,7 @@ if __name__ == "__main__":
         run_dir = os.path.join(args.input_dir, sorted(input_dir_contents)[-1])
     else:
         run_dir = args.input_dir
-    for input_file in os.listdir(run_dir):
+    for input_file in sorted(os.listdir(run_dir)):
         input_path = os.path.join(run_dir, input_file)
         print(f"Loading {input_path}")
         with open(input_path, 'rb') as f:
@@ -35,17 +35,23 @@ if __name__ == "__main__":
             returns = run["returns"]
             if config["library"] not in data:
                 data[config["library"]] = []
+                configs[config["library"]] = []
             data[config["library"]].append(returns)
-            configs[config["library"]] = config
+            configs[config["library"]].append(config)
     
     
-    plt.figure()
+    fig, ax = plt.subplots()
     for library, returns in sorted(filter(lambda x: x[0] in library_lookup, data.items()), key=lambda x: list(library_lookup.keys()).index(x[0])):
         print(f"Library: {library}, Runs: {len(returns)}")
-        print(f"Config: {configs[library]}")
+        print(f"First Config: {configs[library][0]}")
         returns = np.array(data[library])
 
+        seeds_producing_nan = np.arange(len(returns))[np.any(np.isnan(returns), axis=(1, 2))]
+        print(f"Number of seeds producing NaN: {seeds_producing_nan}")
+
+
         returns_aggregate = returns.mean(axis=-1)
+        print(f"returns aggregate: {returns_aggregate}")
 
         returns_min = returns_aggregate.min(axis=0)
         returns_mean = returns_aggregate.mean(axis=0)
@@ -53,16 +59,25 @@ if __name__ == "__main__":
         returns_std = returns_aggregate.std(axis=0)
 
         horizontal = range(0, config["n_steps"], config["evaluation_interval"])
-        plt.fill_between(horizontal, returns_mean - returns_std, returns_mean + returns_std, alpha=0.1)
-        # plt.plot(horizontal, returns_min, label=f"{library_lookup[library]} (min)")
-        # plt.plot(horizontal, returns_median, label=f"{library_lookup[library]} (median)")
-        label = f"{library_lookup[library]} (mean: {returns_mean[-1]:.2f})"
-        plt.plot(horizontal, returns_mean, label=label)
-        # for run in returns_aggregate:
-        #     plt.plot(horizontal, run, alpha=0.1, color="gray")
-        plt.xlabel("Steps")
-        plt.ylabel("Returns")
-        plt.legend()
-    plt.savefig(os.path.join(args.output_dir, "learning_curves.png"))
+        ax.fill_between(horizontal, returns_mean - returns_std, returns_mean + returns_std, alpha=0.1)
+        # ax.plot(horizontal, returns_min, label=f"{library_lookup[library]} (min)")
+        # ax.plot(horizontal, returns_median, label=f"{library_lookup[library]} (median {returns_median[-1]:.2f}, std {returns_std[-1]:.2f})")
+        label = f"{library_lookup[library]} (mean: {returns_mean[-1]:.2f}, std: {returns_std[-1]:.2f})"
+        ax.plot(horizontal, returns_mean, label=label)
+        ax.set_xlabel("Steps")
+        ax.set_ylabel("Returns")
+
+        final_returns = returns[:, -1, :]
+        fig_final_return_distribution, ax_final_return_distribution = plt.subplots()
+        ax_final_return_distribution.hist(final_returns.ravel(), label=label, bins=10)
+        ax_final_return_distribution.legend()
+        ax_final_return_distribution.set_title("Final Return Distribution")
+        ax_final_return_distribution.set_xlabel("Return")
+        ax_final_return_distribution.set_ylabel("Count")
+        fig_final_return_distribution.savefig(os.path.join(args.output_dir, f"final_return_distribution_{library}.png"))
+    ax.legend()
+    fig.savefig(os.path.join(args.output_dir, "learning_curves.png"))
+
+
 
 
