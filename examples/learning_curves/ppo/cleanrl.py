82cf572748cc678c980b84392890f4545a4ea0e8
Jonas Eschmann <jonas.eschmann@gmail.com>
2024-05-06 13:40:42 -0400

    addendum fix

--- examples/learning_curves/ppo/cleanrl.py
+++ examples/learning_curves/ppo/cleanrl.py
@@ -143,7 +143,7 @@ def train_cleanrl(config):
             next_obs, reward, terminations, truncations, infos = envs.step(action.cpu().numpy())
             # next_done = np.logical_or(terminations, truncations)
             rewards[step] = torch.tensor(reward).to(device).view(-1)
-            # next_obs, next_done = torch.Tensor(next_obs).to(device), torch.Tensor(next_done).to(device)
+            next_obs= torch.Tensor(next_obs).to(device)
             terminated[step] = torch.Tensor(terminations)
             truncated[step] = torch.Tensor(np.logical_or(terminations, truncations))
 
