2f9d2b9b8826432a8515e0c511621d37143a9f21
Jonas Eschmann <jonas.eschmann@gmail.com>
2024-05-08 12:16:02 -0400

    bumping rlt

--- examples/learning_curves/td3/tinyrl.py
+++ examples/learning_curves/td3/tinyrl.py
@@ -16,9 +16,9 @@ def env_factory_factory(config, **kwargs):
         return env
     return env_factory
 
-def train_tinyrl(config, use_python_environment=True):
+def train_tinyrl(config, use_python_environment=False):
     custom_environment = {
-        "path": os.path.abspath("../custom_environment"),
+        "path": os.path.join(os.path.dirname(os.path.abspath(__file__)), "../../custom_environment"),
         "action_dim": 1,
         "observation_dim": 3,
     }
@@ -59,7 +59,7 @@ def train_tinyrl(config, use_python_environment=True):
     render = False
     for step_i in range(config["n_steps"]):
         if step_i % config["evaluation_interval"] == 0:
-            current_returns = evaluate_policy(lambda observation: np.tanh(state.action(observation)), config, env_factory, render=config["render"] and step_i >= 0)
+            current_returns = evaluate_policy(lambda observation: state.action(observation), config, env_factory, render=config["render"] and step_i >= 0)
             print(f"Step {step_i}/{config['n_steps']}: {np.mean(current_returns)}", flush=True)
             returns.append(current_returns)
         state.step()
