07421de2445ec18a78b5a8264b4438adcb511b7e
Jonas Eschmann <jonas.eschmann@gmail.com>
2024-05-11 23:24:35 -0400

    big run

--- examples/learning_curves/train.py
+++ examples/learning_curves/train.py
@@ -151,13 +151,13 @@ library_configs = {
         #     "cleanrltrunc": {**ppo.default_config_cleanrltrunc, **environment_configs["PPO"]["Pendulum-v1"]},
         #     "sbx": {**ppo.default_config_sbx, **environment_configs["PPO"]["Pendulum-v1"]}
         # },
-        # "Hopper-v4": {
-        #     "tinyrl": {**ppo.default_config_tinyrl, **environment_configs["PPO"]["Hopper-v4"]},
-        #     "sb3": {**ppo.default_config_sb3, **environment_configs["PPO"]["Hopper-v4"]},
-        #     "cleanrl": {**ppo.default_config_cleanrl, **environment_configs["PPO"]["Hopper-v4"]},
-        #     "cleanrltrunc": {**ppo.default_config_cleanrltrunc, **environment_configs["PPO"]["Hopper-v4"]},
-        #     "sbx": {**ppo.default_config_sbx, **environment_configs["PPO"]["Hopper-v4"]}
-        # },
+        "Hopper-v4": {
+            "tinyrl": {**ppo.default_config_tinyrl, **environment_configs["PPO"]["Hopper-v4"]},
+            "sb3": {**ppo.default_config_sb3, **environment_configs["PPO"]["Hopper-v4"]},
+            "cleanrl": {**ppo.default_config_cleanrl, **environment_configs["PPO"]["Hopper-v4"]},
+            "cleanrltrunc": {**ppo.default_config_cleanrltrunc, **environment_configs["PPO"]["Hopper-v4"]},
+            "sbx": {**ppo.default_config_sbx, **environment_configs["PPO"]["Hopper-v4"]}
+        },
     },
     "TD3":{
         # "Pendulum-v1": {
